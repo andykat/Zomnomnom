5c8e3be52049d9d3ef958776662676e0a7d42f17
andykat <ahtz@mit.edu>
2016-01-09 02:01:48 -0800

    added movement to archon

--- AndrewIsTheBest/RobotPlayer.java
+++ AndrewIsTheBest/RobotPlayer.java
@@ -18,10 +18,26 @@ public class RobotPlayer {
         int myAttackRange = 0;
         Team myTeam = rc.getTeam();
         Team enemyTeam = myTeam.opponent();
-
+        int robotCount=0;
+        int buildingBreak = 0;
+        int buildingBreakConstant = 100;
+        
+        MapLocation archStart = new MapLocation(0,0);
+        MapLocation archEnd = new MapLocation(0,0);
+        
+        int radius = 5;
         if (rc.getType() == RobotType.ARCHON) {
             try {
                 // Any code here gets executed exactly once at the beginning of the game.
+            	int archonCount = rc.getRobotCount();
+            	if(archonCount>3){
+            		buildingBreakConstant = 200;
+            	}
+            	archStart = rc.getLocation();
+            	
+            	archEnd = randomCircleLoc(archStart.x,archStart.y, radius, rand);
+            	
+            	robotCount = 0;
             } catch (Exception e) {
                 // Throwing an uncaught exception makes the robot die, so we need to catch exceptions.
                 // Caught exceptions will result in a bytecode penalty.
@@ -33,43 +49,96 @@ public class RobotPlayer {
                 // This is a loop to prevent the run() method from returning. Because of the Clock.yield()
                 // at the end of it, the loop will iterate once per game round.
                 try {
-
-
+                	boolean flag = false;
                     if (rc.isCoreReady()) {
-                        if (fate < 800) {
-                            // Choose a random direction to try to move in
-                            Direction dirToMove = directions[fate % 8];
-                            // Check the rubble in that direction
-                            if (rc.senseRubble(rc.getLocation().add(dirToMove)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
-                                // Too much rubble, so I should clear it
-                                rc.clearRubble(dirToMove);
-                                // Check if I can move in this direction
-                            } else if (rc.canMove(dirToMove)) {
-                                // Move
-                                rc.move(dirToMove);
-                            }
-                        } else {
-                            // Choose a random unit to build
-                        	
-                            RobotType typeToBuild = robotTypes[1];
-                         
-                            // Check for sufficient parts
-                            if (rc.hasBuildRequirements(typeToBuild)) {
-                                // Choose a random direction to try to build in
-                                Direction dirToBuild = directions[rand.nextInt(8)];
-                                for (int i = 0; i < 8; i++) {
-                                    // If possible, build in this direction
-                                    if (rc.canBuild(dirToBuild, typeToBuild)) {
-                                        rc.build(dirToBuild, typeToBuild);
-                                        break;
-                                    } else {
-                                        // Rotate the direction to try
-                                        dirToBuild = dirToBuild.rotateLeft();
-                                    }
-                                }
-                            }
-                        }
+                    	if(rc.senseNearbyRobots(2, myTeam).length>5){
+                    		//move
+                    		MapLocation ml = rc.getLocation();
+                    		if(Math.abs(ml.x - archEnd.x) + Math.abs(ml.y - archEnd.y) < 3 ){
+                    			archEnd = randomCircleLoc(archStart.x,archStart.y, radius, rand);
+                    		}
+                    		bugMove(rc, rc.getLocation(), archEnd);
+                    		
+                    		
+                    		flag = true;
+                    	}
+                    	else{
+	                    	if(buildingBreak <1){
+		                    	if(robotCount==1){
+		                    		if(rc.hasBuildRequirements(RobotType.SCOUT)){
+		                    			Direction dirToBuild = directions[rand.nextInt(8)];
+		                                for (int i = 0; i < 8; i++) {
+		                                    // If possible, build in this direction
+		                                    if (rc.canBuild(dirToBuild, RobotType.SCOUT)) {
+		                                        rc.build(dirToBuild, RobotType.SCOUT);
+		                                        break;
+		                                    } else {
+		                                        // Rotate the direction to try
+		                                        dirToBuild = dirToBuild.rotateLeft();
+		                                    }
+		                                }
+		                                robotCount++;
+		                                flag = true;
+		                    		}
+		                    	}
+		                    	else if(robotCount==2){
+		                    		if(rc.hasBuildRequirements(RobotType.SOLDIER)){
+		                    			Direction dirToBuild = directions[rand.nextInt(8)];
+		                                for (int i = 0; i < 8; i++) {
+		                                    // If possible, build in this direction
+		                                    if (rc.canBuild(dirToBuild, RobotType.SOLDIER)) {
+		                                        rc.build(dirToBuild, RobotType.SOLDIER);
+		                                        break;
+		                                    } else {
+		                                        // Rotate the direction to try
+		                                        dirToBuild = dirToBuild.rotateLeft();
+		                                    }
+		                                }
+		                    			robotCount++;
+		                    			if(robotCount==5){
+		                    				robotCount=0;
+		                    			}
+		                    			flag = true;
+		                    			buildingBreak = buildingBreakConstant;
+		                    		}
+		                    	}
+		                    	else{
+		                    		if(rc.hasBuildRequirements(RobotType.TURRET)){
+		                    			Direction dirToBuild = directions[rand.nextInt(8)];
+		                                for (int i = 0; i < 8; i++) {
+		                                    // If possible, build in this direction
+		                                    if (rc.canBuild(dirToBuild, RobotType.TURRET)) {
+		                                        rc.build(dirToBuild, RobotType.TURRET);
+		                                        break;
+		                                    } else {
+		                                        // Rotate the direction to try
+		                                        dirToBuild = dirToBuild.rotateLeft();
+		                                    }
+		                                }
+		                    			robotCount++;
+		                    			if(robotCount==5){
+		                    				robotCount=0;
+		                    			}
+		                    			flag = true;
+		                    			buildingBreak = buildingBreakConstant;
+		                    		}
+		                    	}
+	                    	} 
+                    	}
+                    	
+                    	
+	                    if(!flag){
+	                    	//look for parts
+	                    	MapLocation ml = rc.getLocation();
+                    		if(Math.abs(ml.x - archEnd.x) + Math.abs(ml.y - archEnd.y) < 3 ){
+                    			archEnd = randomCircleLoc(archStart.x,archStart.y, radius, rand);
+                    		}
+                    		bugMove(rc, rc.getLocation(), archEnd);
+	                    }
+                    	
                     }
+                        
+                    buildingBreak --;
 
                     Clock.yield();
                 } catch (Exception e) {
@@ -78,111 +147,7 @@ public class RobotPlayer {
                 }
             }
         }
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-        
-        else if(rc.getType() == RobotType.SOLDIER){
-        	try{
-        		myAttackRange = rc.getType().attackRadiusSquared;
-        	}catch(Exception e){
-        		 System.out.println(e.getMessage());
-                 e.printStackTrace();
-        	}
-        	
-        	while (true) {
-                // This is a loop to prevent the run() method from returning. Because of the Clock.yield()
-                // at the end of it, the loop will iterate once per game round.
-        		
-                try {
-                	bugMove(rc, rc.getLocation(), new MapLocation(155,304));
-                	 Clock.yield();
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                    e.printStackTrace();
-                }
-        	}
-        }
-        
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-        
-        else if (rc.getType() != RobotType.TURRET) {
-            try {
-                // Any code here gets executed exactly once at the beginning of the game.
-                myAttackRange = rc.getType().attackRadiusSquared;
-            } catch (Exception e) {
-                // Throwing an uncaught exception makes the robot die, so we need to catch exceptions.
-                // Caught exceptions will result in a bytecode penalty.
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
-
-            while (true) {
-                // This is a loop to prevent the run() method from returning. Because of the Clock.yield()
-                // at the end of it, the loop will iterate once per game round.
-                try {
-                    int fate = rand.nextInt(1000);
-
-                    if (fate % 5 == 3) {
-                        // Send a normal signal
-                        rc.broadcastSignal(80);
-                    }
-
-                    boolean shouldAttack = false;
-
-                    // If this robot type can attack, check for enemies within range and attack one
-                    if (myAttackRange > 0) {
-                        RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(myAttackRange, enemyTeam);
-                        RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(myAttackRange, Team.ZOMBIE);
-                        if (enemiesWithinRange.length > 0) {
-                            shouldAttack = true;
-                            // Check if weapon is ready
-                            if (rc.isWeaponReady()) {
-                                rc.attackLocation(enemiesWithinRange[rand.nextInt(enemiesWithinRange.length)].location);
-                            }
-                        } else if (zombiesWithinRange.length > 0) {
-                            shouldAttack = true;
-                            // Check if weapon is ready
-                            if (rc.isWeaponReady()) {
-                                rc.attackLocation(zombiesWithinRange[rand.nextInt(zombiesWithinRange.length)].location);
-                            }
-                        }
-                    }
-
-                    if (!shouldAttack) {
-                        if (rc.isCoreReady()) {
-                            if (fate < 600) {
-                                // Choose a random direction to try to move in
-                                Direction dirToMove = directions[fate % 8];
-                                // Check the rubble in that direction
-                                if (rc.senseRubble(rc.getLocation().add(dirToMove)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
-                                    // Too much rubble, so I should clear it
-                                    rc.clearRubble(dirToMove);
-                                    // Check if I can move in this direction
-                                } else if (rc.canMove(dirToMove)) {
-                                    // Move
-                                    rc.move(dirToMove);
-                                }
-                            }
-                        }
-                    }
-
-                    Clock.yield();
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                    e.printStackTrace();
-                }
-            }
-        } else if (rc.getType() == RobotType.TURRET) {
+        else if (rc.getType() == RobotType.TURRET || rc.getType() == RobotType.SOLDIER) {
             try {
                 myAttackRange = rc.getType().attackRadiusSquared;
             } catch (Exception e) {
@@ -223,8 +188,34 @@ public class RobotPlayer {
                 }
             }
         }
+        else{
+        	try {
+               
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        	while (true) {
+        		try {
+        			Clock.yield();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+        	
+        	}
+        }
 	}
-	
+	public static MapLocation randomCircleLoc(int x, int y, int radius, Random rand){
+		double dir = rand.nextDouble()*3.14159;
+		int dx = (int)(Math.cos(dir) *((double)radius));
+		int dy = (int)(Math.sin(dir) *((double)radius));
+		if(rand.nextInt(2) == 0){
+			dy = -dy;
+		}
+		
+		return new MapLocation(dx+x,dy+y);
+	}
 	public static void bugMove(RobotController rc, MapLocation start, MapLocation end){
 		try {
 			Direction dir = start.directionTo(end);
@@ -241,7 +232,7 @@ public class RobotPlayer {
 				rc.move(dir);
 			}
 			else{
-				rc.clearRubble(dir);
+				//rc.clearRubble(dir);
 			}
 		} catch (Exception e) {
             System.out.println(e.getMessage());
