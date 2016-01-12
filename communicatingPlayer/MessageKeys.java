b2c76b72e68fbc31b3eb55a840a88e6883ff59f5
Alan Wang <spartanpig@gmail.com>
2016-01-11 17:15:35 -0800

    Cleaning up

--- communicatingPlayer/MessageKeys.java
+++ communicatingPlayer/MessageKeys.java
@@ -3,8 +3,8 @@ package communicatingPlayer;
 public interface MessageKeys {
 	//only 31 bit per integer because largest number for an int is 1 followed by 31 zeros
 	//First four bytes of the integer then is reserved, and last byte is also reserved, the middle 59 bytes are free to use
-	//Message type ranges from 0 ~7, currently 1~7 is occupied
-	//To create a new message type: 1) add in the distibution mapping here, add decrypting and createMessage methods to retrieve
+	//Message type ranges from 0 ~7, currently 0~1 is occupied
+	//To create a new message type: 1) add in the distribution mapping here, add decrypting and createMessage public methods to retrieve
 	
 	public int[] charMesDistr= {11,7,7,15,5,7,7}; //Character message bit distribution
 	public int[] mapMesDistr= {11,7,10,7,15,2,7};
--- communicatingPlayer/MessageTransfer.java
+++ communicatingPlayer/MessageTransfer.java
@@ -5,89 +5,6 @@ import java.util.Vector;
 
 public class MessageTransfer implements MessageKeys{
 	
-	public boolean bigMapTest(){ //Takes a long time, be careful
-		boolean answer= true;
-		int completedTests= 0;
-		outerloop:
-		for (int turn= 0; turn< 2048; turn++){
-			for (int rubble= 0; rubble< 99; rubble++){
-				for (int parts= 0; parts< 999; parts++){
-					for (int denHealth= 0; denHealth< 99; denHealth++){
-						for (int nid= 0; nid< 32000; nid++){
-							for (int ntype=0; ntype< 4; ntype++){
-								for (int nhealth= 0; nhealth< 128; nhealth++){
-									int[] message={turn,rubble,parts,denHealth,nid,ntype,nhealth};
-									boolean test= test(1,message);
-									if (!test){
-										answer= false;
-										break outerloop;
-									}else{
-										completedTests+= 1;
-										System.out.println(Arrays.toString(message));
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			
-		}
-		
-		return answer;
-	}
-	
-	
-	public boolean bigCharacterTest(){
-		boolean answer= true;
-		int completedTests= 0;
-		outerloop:
-		for (int turn= 0; turn< 2048; turn++){
-			for (int x= 0; x< 100; x++){
-				for (int y= 0; y< 100; y++){
-					for(int id= 0; id< 32768; id++ ){
-						for (int charType= 0; charType< 16; charType++){
-							for (int healthPerc= 0; healthPerc< 128; healthPerc++){
-								for (int infectionCount= 0; infectionCount< 128; infectionCount++){
-									int[] message={turn,x,y,id,charType,healthPerc,infectionCount};
-									boolean test= test(0,message);
-									if (!test){
-										answer= false;
-										break outerloop;
-									}else{
-										completedTests+= 1;
-										System.out.println(completedTests);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		
-		return answer;
-	}
-	
-	public boolean test(int messageType, int[] message){
-		boolean answer= true;
-		
-		int[] condensedMessage= createMessage(messageType,message);
-		
-		Vector<Integer> check= decryptMessage(condensedMessage);
-		
-		if (message.length== check.size()){
-			for (int n= 0; n< check.size(); n++){
-				if (message[n]!= check.get(n)){
-					answer= false;
-					break;
-				}
-			}
-		}
-		
-		return answer;
-	}
-	
 	public Vector<Integer> decryptMessage(int[] receivedMessage){
 		//Find message id
 		Vector<Integer> answer= new Vector<Integer>();
@@ -109,20 +26,17 @@ public class MessageTransfer implements MessageKeys{
 				temp[32] = '1';
 				binary = String.valueOf(temp);
 			}
-			//p("binary size: "+binary.length());
 			int startNum= 4;
 			
-			if (messageType== 0){//========================================================================================================Character message
+			if (messageType== 0){ //CHARACTER MESSAGE
 				for (int n= 0; n< MessageKeys.charMesDistr.length; n++){
-					//p(getPart(binary,startNum, startNum+MessageKeys.charMesDistr[n]));
 					System.out.println("\tcharacterType");
 					answer.add(getPart(binary,startNum, startNum+MessageKeys.charMesDistr[n]));
 					startNum+= MessageKeys.charMesDistr[n];
 				}
-			}else if (messageType== 1){
+			}else if (messageType== 1){ //MAP MESSAGE
 				System.out.println("\tmapType");
 				for (int n= 0; n< MessageKeys.mapMesDistr.length; n++){
-					//p(getPart(binary,startNum, startNum+MessageKeys.charMesDistr[n]));
 					answer.add(getPart(binary,startNum, startNum+MessageKeys.mapMesDistr[n]));
 					startNum+= MessageKeys.mapMesDistr[n];
 				}				
@@ -130,10 +44,6 @@ public class MessageTransfer implements MessageKeys{
 		}	
 		
 		return answer;
-	}
-	
-	public int getPart(String binary, int startIndex, int endIndex){
-		return Integer.parseInt(binary.substring(startIndex,endIndex),2);
 	}
 	
 	public int[] createMessage(int idType, int[] message){//Returns -1 on failure
@@ -147,6 +57,10 @@ public class MessageTransfer implements MessageKeys{
 		}
 		
 		return answer;
+	}
+	
+	private int getPart(String binary, int startIndex, int endIndex){
+		return Integer.parseInt(binary.substring(startIndex,endIndex),2);
 	}
 	
 	private int[] createMessage(int idType, int[] distribution, int[]message){
@@ -199,22 +113,106 @@ public class MessageTransfer implements MessageKeys{
 		return answer; //Returns a string representing the correctly padded numbers on success, else returns ""
 	}
 	
-	public String padLeft(String s, int totalLength) {
+	private String padLeft(String s, int totalLength) {
 		//http://stackoverflow.com/questions/4051887/how-to-format-a-java-string-with-leading-zero
 	     if (s.length() >= totalLength) return s;
 	     else return String.format("%0" + (totalLength-s.length()) + "d%s", 0, s);
 	}
 	
-	public String padRight(String s, int totalLength, char c) {
+	private String padRight(String s, int totalLength, char c) {
 		if (s.length()>= totalLength) return s;
 		else return String.format("%-"+totalLength+"s", s).replace(' ', c);
 	}
 	
-	public void p(String s){
+	private void p(String s){
 		System.out.println(s);
 	}
 	
-	public void p(int s){
+	private void p(int s){
 		System.out.println(s);
 	}
+	
+	private boolean bigMapTest(){ //Takes a long time, be careful
+		boolean answer= true;
+		int completedTests= 0;
+		outerloop:
+		for (int turn= 0; turn< 2048; turn++){
+			for (int rubble= 0; rubble< 99; rubble++){
+				for (int parts= 0; parts< 999; parts++){
+					for (int denHealth= 0; denHealth< 99; denHealth++){
+						for (int nid= 0; nid< 32000; nid++){
+							for (int ntype=0; ntype< 4; ntype++){
+								for (int nhealth= 0; nhealth< 128; nhealth++){
+									int[] message={turn,rubble,parts,denHealth,nid,ntype,nhealth};
+									boolean test= test(1,message);
+									if (!test){
+										answer= false;
+										break outerloop;
+									}else{
+										completedTests+= 1;
+										System.out.println(Arrays.toString(message));
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+		}
+		
+		return answer;
+	}
+	
+	
+	private boolean bigCharacterTest(){
+		boolean answer= true;
+		int completedTests= 0;
+		outerloop:
+		for (int turn= 0; turn< 2048; turn++){
+			for (int x= 0; x< 100; x++){
+				for (int y= 0; y< 100; y++){
+					for(int id= 0; id< 32768; id++ ){
+						for (int charType= 0; charType< 16; charType++){
+							for (int healthPerc= 0; healthPerc< 128; healthPerc++){
+								for (int infectionCount= 0; infectionCount< 128; infectionCount++){
+									int[] message={turn,x,y,id,charType,healthPerc,infectionCount};
+									boolean test= test(0,message);
+									if (!test){
+										answer= false;
+										break outerloop;
+									}else{
+										completedTests+= 1;
+										System.out.println(completedTests);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return answer;
+	}
+	
+	private boolean test(int messageType, int[] message){
+		boolean answer= true;
+		
+		int[] condensedMessage= createMessage(messageType,message);
+		
+		Vector<Integer> check= decryptMessage(condensedMessage);
+		
+		if (message.length== check.size()){
+			for (int n= 0; n< check.size(); n++){
+				if (message[n]!= check.get(n)){
+					answer= false;
+					break;
+				}
+			}
+		}
+		
+		return answer;
+	}
 }
+
